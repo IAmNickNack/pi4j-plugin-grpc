a222d25af815180fa8eed5d2026c1cb00d6a4a99
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2025-11-01 15:58:27 +0000

    Optional debug output

--- pi4j-plugin-grpc-server/build.gradle.kts
+++ pi4j-plugin-grpc-server/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation(libs.pi4j.core)
     implementation(libs.pi4j.plugin.ffm)
     implementation(libs.pi4j.plugin.mock)
-    runtimeOnly(libs.logback.classic)
+    implementation(libs.logback.classic)
     testImplementation(libs.grpc.testing)
 }
 
