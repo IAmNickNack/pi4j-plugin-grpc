23b21c74821c7c06fbe111709b7e9667bd5a9886
iamnicknack <5727098+IAmNickNack@users.noreply.github.com>
2026-02-09 08:21:29 +0000

    chore: examples as included build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,5 @@ rootProject.name = "pi4j-plugin-grpc"
 include(":pi4j-plugin-grpc")
 include(":pi4j-plugin-grpc-server")
 
-project(":pi4j-plugin-grpc")
-project(":pi4j-plugin-grpc-server")
-
 includeBuild("build-logic")
 includeBuild("examples")
