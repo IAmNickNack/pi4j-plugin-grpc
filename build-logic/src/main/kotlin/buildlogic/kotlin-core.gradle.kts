2a6aa15389e1fc4e77d07c5b7ce40b0c237802f8
IAmNickNack <5727098+IAmNickNack@users.noreply.github.com>
2026-03-05 21:22:21 +0000

    chore: update dependencies (#30)

--- build-logic/src/main/kotlin/buildlogic/kotlin-core.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic/kotlin-core.gradle.kts
@@ -16,5 +16,12 @@ kotlin {
 dependencies {
     withVersionCatalog {
         implementation(platform(libs.kotlin.bom))
+        implementation("org.jetbrains.kotlin:kotlin-stdlib") {
+            version {
+                // force stdlib to avoid confusion with transitive dependencies
+                strictly(libs.versions.kotlin.asProvider().get())
+            }
+        }
     }
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
